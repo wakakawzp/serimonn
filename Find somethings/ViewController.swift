b687d56c9312b4b63ae36f8b81b755bfce6d4a91
WANG ZHIPENG <wangzhipeng@wangnomacbook-pro.local>
2018-04-07 21:11:27 +0900

    Initial Commit

---
+++ Find somethings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find somethings
+//
+//  Created by WANG ZHIPENG on 2018/04/07.
+//  Copyright © 2018年 WANG ZHIPENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
