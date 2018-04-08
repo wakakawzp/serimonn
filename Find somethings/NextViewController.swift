20339b048547b02f26765b28091674d9b90948cf
WANG ZHIPENG <wangzhipeng@wangnomacbook-pro.local>
2018-04-08 20:53:47 +0900

    Just Basic Structure

---
+++ Find somethings/NextViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NextViewController.swift
+//  Find somethings
+//
+//  Created by WANG ZHIPENG on 2018/04/07.
+//  Copyright © 2018年 WANG ZHIPENG. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func TurntoNext(_ sender: Any) {
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
